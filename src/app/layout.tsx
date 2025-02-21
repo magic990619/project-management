b95076c25b0396df01e8ddadb9f293d0b635e188
magic990619 <magicrjh0619@gmail.com>
2025-02-20 16:11:53 -0800

    feat: implement prettier format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto } from "next/font/google";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Roboto } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
-import "./globals.css";
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
+  variable: '--font-roboto',
+  subsets: ['latin'],
   weight: ['300', '400', '500', '700'],
   display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Project Management",
-  description: "Developed by Drag - 0xMagic",
+  title: 'Project Management',
+  description: 'Developed by Drag - 0xMagic',
 };
 
 export default function RootLayout({
@@ -38,9 +38,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased`}
       >
         <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            {children}
-          </ThemeProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
